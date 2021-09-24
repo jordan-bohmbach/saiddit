00a79043f557b9d98cd97079950af14b26122a55
Jordan Bohmbach <jordan.bohmbach@gmail.com>
2021-09-24 10:24:16 -0500

    updated navbar styling

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -11,6 +11,8 @@ import RedditLogo from '../../images/reddit_logo.png'
 import TwitterLogo from '../../images/twitter_logo.png'
 import WordsLogo from '../../images/words_logo.png'
 
+import SaidditLogo from '../../images/saiddit_logo.png'
+
 const NavBar = () => {
   const user = useSelector(state => state.session.user)
   const dispatch = useDispatch()
@@ -23,40 +25,52 @@ const NavBar = () => {
     <nav className='navbar-container'>
         <div className='logo'>
           <NavLink to='/' exact={true} activeClassName='active'>
-            <img className='nav-logo' src={RedditLogo} alt='logo not found'/>
+            {/* <img className='nav-logo' src={RedditLogo} alt='logo not found'/>
             <img className='nav-logo' src={WordsLogo} alt='logo not found' />
-            <img className='nav-logo' src={TwitterLogo} alt='logo not found' />
+            <img className='nav-logo' src={TwitterLogo} alt='logo not found' /> */}
+            <img className='nav-logo' src={SaidditLogo} alt='logo not found' />
+            <img className='nav-logo' src={WordsLogo} alt='logo not found' />
           </NavLink>
         </div>
-        <div className='search-bar'>
-          <input placeholder='Search'></input>
-        </div>
-        <div>
-          <div className='nav-popular'>
+          <div className='search-bar'>
+            <input placeholder='Search'></input>
+          </div>
+        <div className='nav-icons-container'>
+          <div className='nav-popular nav-icon'>
             <NavLink to='/popular' exact={true} activeClassName='active'>
-              Popular
+              <i class="fas fa-fire"></i>
             </NavLink>
           </div>
-          <div className='nav-create-post'>
+        <div className='nav-all nav-icon'>
+          <NavLink to='/all' exact={true} activeClassName='active'>
+            <i class="fas fa-globe"></i>
+          </NavLink>
+        </div>
+        <div className='nav-chat nav-icon'>
+          <NavLink to='/chat' exact={true} activeClassName='active'>
+            <i class="far fa-comments"></i>
+          </NavLink>
+        </div>
+          <div className='nav-create-post nav-icon'>
             <NavLink to='/posts/new' exact={true} activeClassName='active'>
-              Create Post
+              <i class="far fa-plus-square"></i>
             </NavLink>
           </div>
         </div>
       {user ? '' : 
         <div className='nav-login'>
-          <button onClick={demoLogin}>Demo</button>
-          <NavLink to='/login' exact={true} activeClassName='active'>
+          <button className='nav-login-buttons' onClick={demoLogin}>Demo</button>
+          <NavLink className='nav-login-buttons' to='/login' exact={true} activeClassName='active'>
             Login
           </NavLink>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
+          <NavLink className='nav-login-buttons' to='/sign-up' exact={true} activeClassName='active'>
             Sign Up
           </NavLink>
         </div>}
       {user ? 
         <div className='nav-logout'>
           <LogoutButton />
-          <NavLink to='/users' exact={true} activeClassName='active'>
+          <NavLink className='nav-login-buttons' to='/users' exact={true} activeClassName='active'>
             Users
           </NavLink>
         </div> 
