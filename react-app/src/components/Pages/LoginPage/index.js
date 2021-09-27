91f6a2857727f8350ba117d0cbd8fdc362d1ce59
Jordan Bohmbach <jordan.bohmbach@gmail.com>
2021-09-27 18:09:42 -0500

    styled the login page

--- react-app/src/components/Pages/LoginPage/index.js
+++ react-app/src/components/Pages/LoginPage/index.js
@@ -1,10 +1,28 @@
+import { useDispatch } from "react-redux"
+import { login } from "../../../store/session"
 import LoginForm from "../../auth/LoginForm"
 import './LoginPage.css'
 
 const LoginPage = () => {
+    const dispatch = useDispatch()
+
+    const handleDemo = async () => {
+        await dispatch(login('demo@aa.io', 'password'))
+    }
+
     return(
-        <div className='login-form-container'>
-            <LoginForm />
+        <div className='login-page-container'>
+            <div className='login-form-container'>
+                <h2>Login as an existing user</h2>
+                <LoginForm />
+            </div>
+            <div className='middle-text-section'>
+                <h2>Or ... </h2>
+            </div>
+            <div className='demo-login-section'>
+                <h2>Try things out as Demo User</h2>
+                <button onClick={handleDemo}>Demo</button>
+            </div>
         </div>
     )
 }
