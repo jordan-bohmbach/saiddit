5fa808a624c959071554da59bd4a9971f58388a0
Jordan Bohmbach <jordan.bohmbach@gmail.com>
2021-09-28 17:38:13 -0500

    further styling for the subsaiddit page

--- react-app/src/components/Pages/IndividualSubsaiddit/index.js
+++ react-app/src/components/Pages/IndividualSubsaiddit/index.js
@@ -1,20 +1,53 @@
 import { useSelector } from "react-redux"
 import { useParams } from "react-router"
 import PostTile from "../../PostTile"
+import {Link} from 'react-router-dom'
+
+import './IndividualSubsaiddit.css'
+
+import Spicture from '../../../images/s-slash-picture.png'
+import SideLinksContainer from "../../SideLinksContainer"
 
 const IndividualSubsaiddit = () => {
     const { subsaidditName} = useParams()
     const subsaidditList = useSelector(state=>Object.values(state.subsaiddits))
-    const subsaidditId = subsaidditList.filter(subsaiddit=> subsaiddit.name === subsaidditName)[0]?.id
+    const subsaiddit = subsaidditList.filter(subsaiddit=> subsaiddit.name === subsaidditName)[0]
     const allPosts = useSelector(state=>Object.values(state.posts))
-    const subsaidditPosts = allPosts.filter(post=>post.subsaiddit_id === subsaidditId)
+    const subsaidditPosts = allPosts.filter(post=>post.subsaiddit_id === subsaiddit?.id)
+
+    const userList = useSelector(state=>Object.values(state.users))
+    const moderator = userList.filter(user=>user.id === subsaiddit?.moderator_id)[0]
 
     return (
         <>
-            <h1>{`${subsaidditName} page`}</h1>
-            {subsaidditPosts.map(post=> (
-                <PostTile post={post} />
-            ))}
+            <div className='individual-subsaiddit-banner-image' style={{'background-image': `url(${subsaiddit?.image})`}}></div>
+            <div className='individual-subsaiddit-banner-description'>
+                <img src={Spicture} alt='subsaiddit logo' />
+                <div>
+                    <h1>{subsaiddit?.name}</h1>
+                    <p className='faded-text'>{subsaiddit?.description}</p>
+                </div>
+            </div>
+            <div className='subsaiddit-page-content-container'>
+                <div className='subsaiddit-post-list-container'>
+                    {subsaidditPosts.map(post=> (
+                        <PostTile post={post} />
+                    ))}
+                </div>
+                <div className='individual-subsaiddit-sidebar-container'>
+                    <div className='individual-subsaiddit-rules-container'>
+                        <h1>{`s/${subsaiddit?.name} Rules`}</h1>
+                        <p>{subsaiddit?.rules}</p>
+                    </div>
+                    <div className='individual-subsaiddit-moderator-container'>
+                        <h1>Moderator(s)</h1>
+                        <Link to={`/users/${moderator?.id}`}>
+                            <p>{`u/${moderator?.username}`}</p>
+                        </Link>
+                    </div>
+                    <SideLinksContainer />
+                </div>
+            </div>
         </>
     )
 }
--- react-app/src/components/Pages/Splash/index.js
+++ react-app/src/components/Pages/Splash/index.js
@@ -8,6 +8,7 @@ import { deletePost } from "../../../store/post";
 
 import '../../Style/Style.css'
 import './Splash.css'
+import SideLinksContainer from "../../SideLinksContainer";
 
 const Splash = () => {
     const dispatch = useDispatch()
@@ -79,14 +80,7 @@ const Splash = () => {
                     ))}
                 </div>
 
-                <div className='side-links-container'>
-                    <Link to='/help'>Help</Link>
-                    <Link to='/about'>About</Link>
-                    <Link to='/coins'>Saiddit Coins</Link>
-                    <Link to='/gifts'>Saiddit Gifts</Link>
-                    <Link to='/careers'>Careers</Link>
-                    <Link to='/blog'>Blog</Link>
-                </div>
+                <SideLinksContainer />
             </div>
         </div>
     )
