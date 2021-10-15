8ca868120a28f281505b327fe6e4456205bcf1c2
Jordan Bohmbach <jordan.bohmbach@gmail.com>
2021-10-14 21:26:20 -0500

    added the comments count and post link to the user profile page

---
+++ react-app/src/components/CommentTile/index.js
@@ -0,0 +1,89 @@
+import './CommentTile.css'
+import { updateComment } from '../../store/comment'
+import { useDispatch, useSelector } from 'react-redux'
+import CreateCommentForm from '../Forms/CreateComment'
+import { getComments } from '../../store/comment'
+import { useState } from 'react'
+
+const getTimeString = (timeDifference) => {
+    const daysDifference = Math.floor(timeDifference / 86400000)
+    const hoursDifference = Math.floor((timeDifference % 86400000) / 3600000)
+    const minutesDifference = Math.round(((timeDifference % 86400000) % 3600000) / 60000)
+
+    let timeDifferenceString = ''
+    if (daysDifference > 0) {
+        if (daysDifference === 1) {
+            timeDifferenceString += `${daysDifference} Day`
+        } else {
+            timeDifferenceString += `${daysDifference} Days`
+        }
+    }
+    if (hoursDifference > 0) {
+        if (hoursDifference === 1) {
+            timeDifferenceString += ` ${hoursDifference} Hour`
+        } else {
+            timeDifferenceString += ` ${hoursDifference} Hours`
+        }
+    }
+    if (minutesDifference > 0) {
+        if (minutesDifference === 1) {
+            timeDifferenceString += ` ${minutesDifference} Minute`
+        } else {
+            timeDifferenceString += ` ${minutesDifference} Minutes`
+        }
+    }
+    if (minutesDifference === 0 && daysDifference === 0 && hoursDifference === 0) {
+        timeDifferenceString += 'Less than a Minute '
+    }
+    return '- ' + timeDifferenceString + ' Ago'
+}
+
+
+const CommentTile = ({comment}) => {
+    const dispatch = useDispatch()
+    const [editing, setEditing] = useState(false)
+
+    let timeDifference
+    if (process.env.NODE_ENV === 'production') {
+        timeDifference = (new Date() - Date.parse(comment.createdat))
+    } else {
+        timeDifference = (new Date() - Date.parse(comment.createdat)) - 18000000
+    }
+
+    const handleDelete = async (e) => {
+        // dispatch(deleteComment(e.target.value))
+        const payload = {
+            id : comment.id,
+            user_id: 2,
+            post_id : comment.post_id,
+            parent_id : comment.parent_id,
+            content: 'Deleted',
+            createdat: comment.createdat,
+            updatedat: new Date()
+        }
+
+        let updatedComment = await dispatch(updateComment(payload))
+        if (updatedComment) {
+            getComments()
+        }
+    }
+
+    const handleEdit = (e) => {
+        setEditing(true)
+    }
+
+
+    return (
+        <>
+            <div>Commented {getTimeString(timeDifference)}</div>
+            <h3>{comment.content}</h3>
+            <div className='individual-comment-buttons-container'>
+            <button value={comment.id} onClick={handleDelete}>Delete</button>
+            <button value={comment.id} onClick={handleEdit}>Edit</button>
+            {editing ? <CreateCommentForm editing={editing} setEditing={setEditing} updateingId={comment.id} /> : ''}
+            </div>
+        </>
+    )
+}
+
+export default CommentTile
--- react-app/src/components/User.js
+++ react-app/src/components/User.js
@@ -5,6 +5,8 @@ import PostTile from './PostTile';
 import SubsaidditList from './SubsaidditList';
 import { useHistory } from 'react-router';
 import { deletePost } from '../store/post';
+import CommentTile from './CommentTile';
+import { Link } from 'react-router-dom';
 
 function User() {
   const [user, setUser] = useState({});
@@ -12,6 +14,7 @@ function User() {
   const history = useHistory()
   const dispatch = useDispatch()
   const loggedInUser = useSelector(state=>state.session.user)
+  const allComments = useSelector(state => Object.values(state.comments))
 
   useEffect(() => {
     if (!userId) {
@@ -26,9 +29,12 @@ function User() {
 
   const postList = useSelector(state=>Object.values(state.posts))
   const subsaidditList = useSelector(state=>Object.values(state.subsaiddits))
+  const commentList = useSelector(state=>Object.values(state.comments))
 
   const userPosts = postList.filter(post=>post.owner_id === user?.id)
   const userSubsaiddits = subsaidditList.filter(subsaiddit=>subsaiddit.owner_id === parseInt(userId))
+  const userComments = commentList.filter(comment=>comment.user_id===parseInt(userId))
+  
 
   const handlePostEdit = (e) => {
     history.push(`/posts/${e.target.value}/edit`)
@@ -51,14 +57,25 @@ function User() {
         </div>
         {userPosts.map(post=>(
           <div className='outer-post-container' key={post.id}>
-            <PostTile post={post} />
-            <div className='post-modification-buttons'>
-                {loggedInUser?.id === post?.owner_id ? <button value={post.id} onClick={handlePostEdit}>Edit Post</button> : ''}
-                {loggedInUser?.id === post?.owner_id ? <button value={post.id} onClick={handlePostDelete}>Delete Post</button> : ''}
-            </div>
+            <Link className='post-tile-container-link' key={post.id} to={`/posts/${post.id}`}>
+              <PostTile post={post} />
+                <div className='post-interaction-button-container'>
+                  <div className='post-interaction-button'>
+                      <i className="far fa-comment-dots"></i>
+                      <p>{`${allComments.filter(comment=>comment.post_id === post.id).length}`} Comments</p>
+                  </div>
+                </div>
+            </Link>
+              <div className='post-modification-buttons'>
+                  {loggedInUser?.id === post?.owner_id ? <button value={post.id} onClick={handlePostEdit}>Edit Post</button> : ''}
+                  {loggedInUser?.id === post?.owner_id ? <button value={post.id} onClick={handlePostDelete}>Delete Post</button> : ''}
+              </div>
           </div>
         ))}
-          {!userPosts.length && <div className='user-no-posts-filler'><h1>No Posts or Comments to Show!</h1></div>}
+        {!userPosts.length && <div className='user-no-posts-filler'><h1>No Posts or Comments to Show!</h1></div>}
+        {/* {userComments.map(comment=>(
+          <CommentTile comment={comment} />
+        ))} */}
       </div>
 
       <div className='user-profile-subsaiddts-container'>
