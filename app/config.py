2fc9bb8d97484de139932634ab288f8ce618e2a7
Jordan Bohmbach <jordan.bohmbach@gmail.com>
2021-10-28 16:32:02 -0500

    fixed bug causing console warning about http request type

--- app/config.py
+++ app/config.py
@@ -10,4 +10,4 @@ class Config:
     S3_BUCKET = os.environ.get("S3_BUCKET_NAME")
     S3_KEY = os.environ.get("S3_ACCESS_KEY")
     S3_SECRET = os.environ.get("S3_SECRET_ACCESS_KEY")
-    S3_LOCATION = f"http://{S3_BUCKET}.s3.amazonaws.com/"
+    S3_LOCATION = f"https://{S3_BUCKET}.s3.amazonaws.com/"
